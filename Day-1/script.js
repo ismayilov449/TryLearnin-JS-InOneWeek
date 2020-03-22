4af93868aec4b4878d26a4758b404af44fa3098a
Rufat Ismayilov <rufet.ismayilov.1999@gmail.com>
2020-03-22 20:18:40 +0400

    -Finished-Variable types and their methods

---
+++ Day-1/script.js
@@ -0,0 +1,110 @@
+
+// var customer =  {
+//     name:'Rufat',
+//     surName:'Ismayilov',
+//     userID:'AZE15512687',
+//     orderCost:500.98,
+//     gender:'Male',
+//     address:'Baku/AZE',
+//     hobbies:['Programming','Hookah','Football']
+
+// }
+
+// var order1 = 100.8;
+// var order2 = 200.5;
+
+// var totalOrder = parseFloat(order1) + parseInt(order2);
+
+// var birthYear = 1999;
+
+
+
+// alert( Number(new Date().getFullYear()) - Number(birthYear) );
+
+// let val;
+
+// const a = 10;
+// const b = 5;
+
+
+// val = a+b;
+
+// var can = {
+//     height: 1.86,
+//     weight: 90
+// }
+
+// var ada = {
+//     height: 1.72,
+//     weight: 53
+// }
+
+// var indexofcan = can.weight / Math.pow(can.height, 2);
+// var indexofada = ada.weight / Math.pow(ada.height, 2);
+ 
+
+// var calculateIndex = function (index) {
+
+//     if (index > 0 && index < 18.4) {
+//         return Number(index).toFixed(2) + '    Okay';
+//     } else if (index > 18.5 && index < 24.9) {
+//         return Number(index).toFixed(2) + '    Normal';
+//     } else if (index > 25 && index < 29.9) {
+//         return Number(index).toFixed(2) + '    Not Normal';
+//     } else {
+//         return Number(index).toFixed(2) + '    Over weight';
+//     }
+
+// };
+
+// console.log("Can : " + calculateIndex(indexofcan));
+// console.log("Ada : " + calculateIndex(indexofada));
+
+
+// let today = new Date();
+
+// console.log(today.getDay() + "/" + today.getMonth() + "/" + today.getFullYear());
+
+// var sentence = "    Template Literals or template strings is the ability Have multi-line strings without any funny business.    ";
+// // var words = sentence.split(' ');
+// var index = sentence.indexOf('-');
+// sentence = sentence.
+// sentence = sentence.trim();
+// console.log(sentence);
+
+
+
+var cars = ['BMW','Mercedes','Opel','Mazda'];
+
+// console.log(cars.length);
+// console.log(cars[0],cars[cars.length-1]);
+
+//  cars.push('Renault')
+// // console.log(cars);
+
+//  cars.unshift('Toyota')
+// // console.log(cars);
+
+
+// console.log(cars);
+// cars.pop('Renault')
+// console.log(cars);
+// cars.shift('Toyota');
+// console.log(cars);
+
+// cars = cars.reverse()
+// console.log(cars);
+
+// cars = cars.sort();
+// console.log(cars);
+
+
+var str = 'vaz,qaz';
+var arr = str.split(',');
+console.log(arr);
+
+var newarr = arr.concat(cars);
+console.log(newarr);
+
+var newarr = newarr.slice(0,newarr.length-2);
+console.log(newarr);
