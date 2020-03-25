1833c19ce9fe741be58c5ec123f4e6f7f23e9f97
Rufat Ismayilov <rufet.ismayilov.1999@gmail.com>
2020-03-25 14:45:45 +0400

    Getters&Setters/Call,Apply,Bind/ErrorHandling(Try&Catch)

--- JS/objects.js
+++ JS/objects.js
@@ -151,61 +151,125 @@
 
 
 
-let Person = function (name, year) {
-    this.name = name;
-    this.year = year;
+// let Person = function (name, year) {
+//     this.name = name;
+//     this.year = year;
+// }
+
+// Person.prototype.Introduce = function () {
+//     console.log('Hi,I am ' + this.name);
+// }
+
+// let Student = function (name, year, number) {
+//     Person.call(this, name, year);
+//     this.number = number;
+// }
+
+// Student.prototype = Object.create(Person.prototype);
+// Student.prototype.constructor = Student;
+
+// Student.prototype.Study = function () {
+//     console.log(number + ' ' + number + ' is learning now!');
+// }
+
+
+// let Teacher = function (name, year, branch) {
+
+//     Person.call(this, name, year);
+//     this.branch = branch;
+
+// }
+
+// Teacher.prototype = Object.create(Person.prototype);
+// Teacher.prototype.constructor = Teacher;
+
+// Teacher.prototype.Teach = function () {
+//     console.log(name + ' ' + this.branch + ' is teaching now!');
+// }
+
+// let Headmaster = function (name, year, branch) {
+
+//     Teacher.call(this,name, year, branch);
+
+// }
+
+// Headmaster.prototype = Object.create(Teacher.prototype);
+// Headmaster.prototype.constructor = Headmaster;
+
+// Headmaster.prototype.ShareTask = function () {
+//     console.log('Headmaster ' + this.name + ' is sharing tasks now!');
+// }
+
+// let rufat = new Headmaster('Rufat',1999,'math');
+// rufat.Introduce();
+
+// rufat.ShareTask();
+
+// const person = {
+//     name: 'Rufat',
+//     surName: 'Ismayilov',
+
+
+// };
+
+
+// Object.defineProperty(person, 'fullName', {
+//     get function() {
+//         console.log(name + ' ' + surNme);
+//     },
+//     set function(value) {
+//         const parts = value.split(' ');
+//         this.name = parts[0];
+//         this.surName = parts[1];
+//     }
+
+// })
+
+// Object.defineProperty(person, 'Age', {
+//     value: 50,
+//     writable : true
+// })
+
+// //Person.setFullName = 'Adam adamli';
+
+// console.log(person);
+
+// var welcome = function (a,b) {
+//     console.log('Welcome ' + this.name + '.Are u interested in '+ a + ' and ' + b );
+// }
+
+// var adam = {name : 'Adam'}
+
+// welcome.call(adam,'asp.net','react');
+
+// // console.log(welcome
+
+// welcome.apply(adam,['asp.net','angular']);
+
+// welcomeAdam = welcome.bind(adam);
+
+// welcomeAdam('asp.net','css');
+
+
+// var range = {
+//     max: prompt('enter the max number'),
+//     min: prompt('enter the min number')
+// }
+
+// var rangeFunction = function (b) {
+//     b = parseInt(b);
+//     if (b <= this.max && b >= this.min) {
+
+//         return console.log('aue');
+//     }
+// }
+
+
+// rangeFunction.call(range, prompt('enter your number : '));
+
+try {
+
+    console.log(myFunction());
+} catch (error) {
+    console.log(error);
 }
-
-Person.prototype.Introduce = function () {
-    console.log('Hi,I am ' + this.name);
-}
-
-let Student = function (name, year, number) {
-    Person.call(this, name, year);
-    this.number = number;
-}
-
-Student.prototype = Object.create(Person.prototype);
-Student.prototype.constructor = Student;
-
-Student.prototype.Study = function () {
-    console.log(number + ' ' + number + ' is learning now!');
-}
-
-
-let Teacher = function (name, year, branch) {
-
-    Person.call(this, name, year);
-    this.branch = branch;
-
-}
-
-Teacher.prototype = Object.create(Person.prototype);
-Teacher.prototype.constructor = Teacher;
-
-Teacher.prototype.Teach = function () {
-    console.log(name + ' ' + this.branch + ' is teaching now!');
-}
-
-let Headmaster = function (name, year, branch) {
-
-    Teacher.call(this,name, year, branch);
-
-}
-
-Headmaster.prototype = Object.create(Teacher.prototype);
-Headmaster.prototype.constructor = Headmaster;
-
-Headmaster.prototype.ShareTask = function () {
-    console.log('Headmaster ' + this.name + ' is sharing tasks now!');
-}
-
-let rufat = new Headmaster('Rufat',1999,'math');
-rufat.Introduce();
-
-rufat.ShareTask();
-
-
-
-
-
